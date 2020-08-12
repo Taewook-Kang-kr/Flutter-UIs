33dd0feee2cf265744b1ce2a419ec57ca0053224
Taewook Kang <57892261+Taewook-Kang-kr@users.noreply.github.com>
2020-08-12 22:46:56 +0900

    Covid19 Statistics UI

---
+++ covid_statistics_UI/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'screens/bottom_nav_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Coivd-19 Dashboard',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: Colors.white,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: BottomNavScreen(),
+    );
+  }
+}
+
+
