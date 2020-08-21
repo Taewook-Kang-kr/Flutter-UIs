86e662f9117d9caab231f690232f48ba9f9ba5f5
Taewook Kang <57892261+Taewook-Kang-kr@users.noreply.github.com>
2020-08-21 22:53:12 +0900

    Neumorphism UIs
    - Music player app UI
    - Calculator UI

---
+++ neumorphism_UIs/lib/audio_player_screen.dart
@@ -0,0 +1,277 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_neumorphic/flutter_neumorphic.dart';
+
+class AudioPlayerScreen extends StatefulWidget {
+  @override
+  _AudioPlayerScreenState createState() => _AudioPlayerScreenState();
+}
+
+class _AudioPlayerScreenState extends State<AudioPlayerScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return NeumorphicTheme(
+      themeMode: ThemeMode.light,
+      theme: NeumorphicThemeData(
+        defaultTextColor: Color(0xFF3E3E3E),
+        baseColor: Color(0xFFDDE6E8),
+        intensity: 0.5,
+        lightSource: LightSource.topLeft,
+        depth: 10,
+      ),
+      darkTheme: neumorphicDefaultDarkTheme.copyWith(
+        defaultTextColor: Colors.white70,
+      ),
+      child: _Page(),
+    );
+  }
+}
+
+class _Page extends StatefulWidget {
+  @override
+  __PageState createState() => __PageState();
+}
+
+class __PageState extends State<_Page> {
+  bool _useDarkMode = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: NeumorphicBackground(
+          child: Column(
+            children: [
+              SizedBox(height: 14),
+              _buildTopBar(context),
+              SizedBox(height: 80),
+              _buildImage(context),
+              SizedBox(height: 30),
+              _buildTitle(context),
+              SizedBox(height: 40),
+              _buildSeekBar(context),
+              SizedBox(height: 20),
+              _buildControlBar(context),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildTopBar(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          Align(
+            alignment: Alignment.centerLeft,
+            child: NeumorphicButton(
+              padding: const EdgeInsets.all(18.0),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              style: NeumorphicStyle(
+                shape: NeumorphicShape.flat,
+                boxShape: NeumorphicBoxShape.circle(),
+              ),
+              child: Icon(
+                Icons.navigate_before,
+                color: _iconsColor(),
+              ),
+            ),
+          ),
+          Align(
+            alignment: Alignment.center,
+            child: Text(
+              "Now Playing",
+              style: TextStyle(
+                fontSize: 23.0,
+                fontWeight: FontWeight.bold,
+                color: NeumorphicTheme.defaultTextColor(context),
+              ),
+            ),
+          ),
+          Align(
+            alignment: Alignment.centerRight,
+            child: NeumorphicButton(
+              padding: const EdgeInsets.all(18.0),
+              onPressed: () {
+                setState(() {
+                  _useDarkMode = !_useDarkMode;
+                  NeumorphicTheme.of(context).themeMode =
+                      _useDarkMode ? ThemeMode.dark : ThemeMode.light;
+                });
+              },
+              style: NeumorphicStyle(
+                shape: NeumorphicShape.flat,
+                boxShape: NeumorphicBoxShape.circle(),
+              ),
+              child: Icon(
+                Icons.favorite_border,
+                color: _iconsColor(),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _buildImage(BuildContext context) {
+    return Neumorphic(
+      style: NeumorphicStyle(
+        boxShape: NeumorphicBoxShape.circle(),
+      ),
+      child: Container(
+        height: 200,
+        width: 200,
+        child: Image.asset(
+          "assets/images/IU.jpg",
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildTitle(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Text(
+          "Blueming",
+          style: TextStyle(
+            fontWeight: FontWeight.w800,
+            fontSize: 32,
+            color: NeumorphicTheme.defaultTextColor(context),
+          ),
+        ),
+        SizedBox(height: 5),
+        Text(
+          "아이유",
+          style: TextStyle(
+            fontWeight: FontWeight.w400,
+            fontSize: 14,
+            color: NeumorphicTheme.defaultTextColor(context),
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget _buildSeekBar(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 28.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          NeumorphicSlider(
+            height: 8,
+            min: 0,
+            max: 314,
+            value: 100,
+            onChanged: (value) {},
+          ),
+          SizedBox(height: 15),
+          Stack(
+            children: [
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Text("1:10",
+                    style: TextStyle(
+                      color: NeumorphicTheme.defaultTextColor(context),
+                    )),
+              ),
+              Align(
+                alignment: Alignment.centerRight,
+                child: Text("3:14",
+                    style: TextStyle(
+                      color: NeumorphicTheme.defaultTextColor(context),
+                    )),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildControlBar(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        NeumorphicButton(
+            padding: const EdgeInsets.all(12.0),
+            onPressed: () {},
+            style: NeumorphicStyle(
+              shape: NeumorphicShape.flat,
+              boxShape: NeumorphicBoxShape.circle(),
+            ),
+            child: Icon(
+              Icons.shuffle,
+              color: _iconsColor(),
+            )),
+        const SizedBox(width: 12),
+        NeumorphicButton(
+            padding: const EdgeInsets.all(18.0),
+            onPressed: () {},
+            style: NeumorphicStyle(
+              shape: NeumorphicShape.flat,
+              boxShape: NeumorphicBoxShape.circle(),
+            ),
+            child: Icon(
+              Icons.skip_previous,
+              color: _iconsColor(),
+            )),
+        const SizedBox(width: 12),
+        NeumorphicButton(
+          padding: const EdgeInsets.all(24.0),
+          onPressed: () {},
+          style: NeumorphicStyle(
+            shape: NeumorphicShape.flat,
+            boxShape: NeumorphicBoxShape.circle(),
+          ),
+          child: Icon(
+            Icons.play_arrow,
+            size: 42,
+            color: _iconsColor(),
+          ),
+        ),
+        const SizedBox(width: 12),
+        NeumorphicButton(
+          padding: const EdgeInsets.all(18.0),
+          onPressed: () {},
+          style: NeumorphicStyle(
+            shape: NeumorphicShape.flat,
+            boxShape: NeumorphicBoxShape.circle(),
+          ),
+          child: Icon(
+            Icons.skip_next,
+            color: _iconsColor(),
+          ),
+        ),
+        const SizedBox(width: 12),
+        NeumorphicButton(
+            padding: const EdgeInsets.all(12.0),
+            onPressed: () {},
+            style: NeumorphicStyle(
+              shape: NeumorphicShape.flat,
+              boxShape: NeumorphicBoxShape.circle(),
+            ),
+            child: Icon(
+              Icons.repeat,
+              color: _iconsColor(),
+            )),
+      ],
+    );
+  }
+
+  Color _iconsColor() {
+    final theme = NeumorphicTheme.of(context);
+    if (theme.isUsingDark) {
+      return theme.current.accentColor;
+    } else {
+      return null;
+    }
+  }
+}
---
+++ neumorphism_UIs/lib/calculator_screen.dart
@@ -0,0 +1,235 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_neumorphic/flutter_neumorphic.dart';
+
+class CalculatorScreen extends StatefulWidget {
+  @override
+  _CalculatorScreenState createState() => _CalculatorScreenState();
+}
+
+final Color _calTextColor = Color(0xFF484848);
+
+class _CalculatorScreenState extends State<CalculatorScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return NeumorphicTheme(
+      theme: NeumorphicThemeData(
+        baseColor: Color(0xFFF4F5F5),
+        intensity: 0.8,
+        lightSource: LightSource.topLeft,
+        variantColor: Colors.red,
+        accentColor: Colors.redAccent,
+        depth: 4,
+      ),
+      child: Scaffold(
+        body: SafeArea(
+          child: NeumorphicBackground(
+            child: _PageContent(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _PageContent extends StatefulWidget {
+  @override
+  __PageContentState createState() => __PageContentState();
+}
+
+class __PageContentState extends State<_PageContent> {
+  final buttons = [
+    CalcButton("%", textAccent: true),
+    CalcButton("^", textAccent: true),
+    CalcButton("sqr", textAccent: true),
+    CalcButton("C", textAccent: true),
+
+    CalcButton("7"),
+    CalcButton("8"),
+    CalcButton("9"),
+    CalcButton("/", textAccent: true),
+
+    CalcButton("4"),
+    CalcButton("5"),
+    CalcButton("6"),
+    CalcButton("X", textAccent: true),
+
+    CalcButton("1"),
+    CalcButton("2"),
+    CalcButton("3"),
+    CalcButton("-", textAccent: true),
+
+    CalcButton("0"),
+    CalcButton("."),
+    CalcButton("=", backgroundAccent: true),
+    CalcButton("+", textAccent: true),
+  ];
+
+
+  @override
+  Widget build(BuildContext context) {
+    return NeumorphicBackground(
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          Align(
+            alignment: Alignment.topLeft,
+            child: Padding(
+              padding: const EdgeInsets.only(left: 18.0, top: 8),
+              child: NeumorphicButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                style: NeumorphicStyle(
+                  shape: NeumorphicShape.flat,
+                  boxShape: NeumorphicBoxShape.circle(),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(3.0),
+                  child: Icon(Icons.navigate_before,),
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Padding(
+              padding: const EdgeInsets.all(18.0),
+              child: _TopScreenWidget(),
+            ),
+          ),
+          Expanded(
+            flex: 2,
+            child: GridView.count(
+              crossAxisCount: 4,
+              padding: const EdgeInsets.only(left: 40.0, right: 40.0),
+              children: List.generate(buttons.length, (index) {
+                return WidgetCalcButton(buttons[index]);
+              }),
+            ),
+          ),
+          // Row(
+          //   children: [
+          //     RaisedButton(
+          //       onPressed: () {
+          //         setState(() {
+          //           NeumorphicTheme.of(context).updateCurrentTheme(NeumorphicThemeData(
+          //             depth:4,
+          //             intensity: 0.8,
+          //             accentColor: Colors.cyan,
+          //           ));
+          //         });
+          //       },
+          //       child: Text("style 1"),
+          //     ),
+          //     RaisedButton(
+          //       onPressed: () {
+          //         setState(() {
+          //           NeumorphicTheme.of(context).updateCurrentTheme(NeumorphicThemeData(
+          //             depth:4,
+          //             intensity: 0.8,
+          //             accentColor: Colors.redAccent,
+          //           ));
+          //         });
+          //       },
+          //       child: Text("style 2"),
+          //     ),
+          //   ],
+          // )
+        ],
+      ),
+    );
+  }
+}
+
+class CalcButton {
+  final String label;
+  final bool textAccent;
+  final bool textVariant;
+  final bool backgroundAccent;
+
+  CalcButton(this.label,
+      {this.textAccent = false,
+      this.textVariant = false,
+      this.backgroundAccent = false});
+}
+
+class WidgetCalcButton extends StatelessWidget {
+  final CalcButton button;
+
+  WidgetCalcButton(this.button);
+
+  Color _textColor(BuildContext context) {
+    if (button.backgroundAccent) {
+      return Colors.white;
+    } else if (button.textAccent) {
+      return NeumorphicTheme.accentColor(context);
+    } else if (button.textVariant) {
+      return NeumorphicTheme.variantColor(context);
+    } else {
+      return _calTextColor;
+    }
+  }
+
+  Color _backgroundColor(BuildContext context) {
+    return button.backgroundAccent
+        ? NeumorphicTheme.accentColor(context)
+        : null;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 14),
+      child: NeumorphicButton(
+        onPressed: () {},
+        style: NeumorphicStyle(
+            surfaceIntensity: 0.15,
+            boxShape: NeumorphicBoxShape.circle(),
+            shape: NeumorphicShape.concave,
+            color: _backgroundColor(context)),
+        child: Center(
+          child: Text(
+            button.label,
+            style: TextStyle(
+              fontSize: 24,
+              color: _textColor(context),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _TopScreenWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Neumorphic(
+      style: NeumorphicStyle(
+        boxShape: NeumorphicBoxShape.roundRect(BorderRadius.circular(12)),
+        depth: -1 * NeumorphicTheme.of(context).current.depth,
+      ),
+      child: FractionallySizedBox(
+        widthFactor: 1,
+        child: Padding(
+          padding: const EdgeInsets.all(18.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Text(
+                "3 x 7 =",
+                style: TextStyle(fontSize: 30, color: _calTextColor),
+              ),
+              Text(
+                "21",
+                style: TextStyle(fontSize: 56, color: _calTextColor),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
